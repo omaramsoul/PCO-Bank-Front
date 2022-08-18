02e5eb8c98c353de49ebe690ecec9020e91065b5
omar laaziri <olaaziri@gmail.com>
2022-08-18 16:34:03 +0200

    mise en forme en cours...

--- src/app/demo/view/Formatter.ts
+++ src/app/demo/view/Formatter.ts
@@ -7,19 +7,19 @@ export class Formatter{
         let colorMapper =  new Map<any, String>();
 
 
-// au debut on cherche les "#..#" qui signifient que l'entete du tableau contient un code de formattage
+            // chercher si l'entete du tableau contient un code de formattage
             for(let i = 0; i < data[0].length; i++){
 
-                if (data[0][i].match(/(?<=#)(.*?)(?=#)/g) !== null){
+            if (data[0][i].match(/(?<=#)(.*?)(?=#)/g) !== null){
 
-// si l'entete de la table contient un code de formattage ( e.g: le check ne donne pas un resultat null, on procede vers le formattage de la colonne correspondante)
+                    // si l'entete de la table contient un code de formattage ( e.g: le check ne donne pas un resultat null, on procede vers le formattage de la colonne correspondante)
                     let codeFormattage = data[0][i].match(/(?<=#)(.*?)(?=#)/g).toString();
+                     // enlever l'encodage de l'entete
+                    data[0][i] =data[0][i].replace(/#(.*?)#/g, "");
 
             switch (codeFormattage){
                 case 'JJMMAA':
-                            data[0][i] =data[0][i].replace(/#(.*?)#/g, ""); // enlever l'encodage de l'entete
-
-                            //formattage de la date
+                            //format de la date
                             for(let j = 1; j < data.length ; j++){
                                 if(data[j][i] === 19000000) data[j][i] = "";
 
@@ -27,11 +27,11 @@ export class Formatter{
                             }
                             break;
                 case 'JJMMAA+':
-                            data[0][i] =data[0][i].replace(/#(.*?)#/g, ""); // enlever l'encodage de l'entete
-                           
-                            //formattage de la date
+                            //format de la date
                             for(let j = 1; j < data.length ; j++){
+                                // si la date est vide, quand la valeur est = 19,000,000
                                 if(data[j][i] === 19000000){ data[j][i] = ""; }
+
                                 else{ 
                                     const current_date = new Date();
                                     const data_date = new Date(data[j][i].toString().slice(0, 4) +"-" +data[j][i].toString().slice(4, 6) + "-" + data[j][i].toString().slice(6, 8));
@@ -40,10 +40,21 @@ export class Formatter{
                                     else {colorMapper.set(data[j][i], "#FF0000");}
 
                             
-                                    }
+                                }
 
                             }
                             break;
+                        case 'JJMM': 
+                                     //format de la date
+                                    for(let j = 1; j < data.length ; j++){
+                                        // si la date est vide, quand la valeur est = 19,000,000
+                                        if(data[j][i] === 19000000){ data[j][i] = ""; }
+
+                                        else{
+                                        
+
+                                        }
+
                     
 
 
@@ -57,7 +68,9 @@ export class Formatter{
             
             
                 }
+            }
                 return colorMapper;
         }
 
 }
+
