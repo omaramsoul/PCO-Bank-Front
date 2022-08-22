91dc4589a2c6bf1a558e4a44608e16b0e38e3187
omar laaziri <olaaziri@gmail.com>
2022-08-22 12:33:52 +0200

    push for documentation

--- src/app/demo/view/comptes/comptes.component.ts
+++ src/app/demo/view/comptes/comptes.component.ts
@@ -9,11 +9,46 @@ import { ComptesService } from '../../service/comptes.service';
 export class ComptesComponent implements OnInit {
 
   comptes: any;
-  selectedCompte: any
+  selectedCompte: any;
+  lineStylesData: any;
+  basicOptions: any;
+  basicData: any;
   constructor(private service:ComptesService) { }
 
   ngOnInit(): void {
-    this.comptes = this.service.getComptes().subscribe(data => this.comptes = data);
+    this.comptes = this.service.getComptes().subscribe(data =>{ 
+      this.comptes = data
+      this.comptes.shift()
+    
+    });
+    this.basicData = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+          {
+              label: 'First Dataset',
+              data: [65, 59, 80, 81, 56, 55, 40],
+              fill: false,
+              borderColor: '#42A5F5',
+              tension: .4
+          }
+      ]
+    };
+    this.lineStylesData = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+          {
+              label: 'First Dataset',
+              data: [65, 59, 80, 81, 56, 55, 40],
+              fill: false,
+              tension: .4,
+              borderColor: '#42A5F5'
+          },
+      ]
+  };
+  
+
+
+
   }
 
 }
